25c093316c77dbe1463c875ce141b7d3f571b80a
Thijs ter Haar <thijsterhaar@home.nl>
2015-05-20 21:40:29 +0200

    Fix broken merge

--- include/DuckInterpreter.h
+++ include/DuckInterpreter.h
@@ -3,11 +3,8 @@
 
 #include <iostream>
 #include <map>
-<<<<<<< HEAD
 #include <stack>
-=======
 #include <list>
->>>>>>> e0423b7dd934f3f1eee23f3c63a1c9d4609a56db
 
 using namespace std;
 
@@ -21,13 +18,10 @@ class DuckInterpreter
         void interpretChar(char);
     protected:
         map<char, FunctionFunctor*> functions;
-<<<<<<< HEAD
         enum InterpretingType{Ignore, IgnoreAll, Accept};
         std::stack<InterpretingType> interpretingState;
-=======
         list<double> dataStack;
         void specialCharHandle(char);
->>>>>>> e0423b7dd934f3f1eee23f3c63a1c9d4609a56db
     private:
 };
 
